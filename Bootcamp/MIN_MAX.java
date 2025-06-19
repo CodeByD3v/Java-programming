0a68b9f90e189ef2142729c6c31ea0fc0edf7957
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2025-06-19 15:00:10 +0530

    Add files via upload

--- Bootcamp/MIN_MAX.java
+++ Bootcamp/MIN_MAX.java
@@ -1,10 +1,10 @@
-// finding the maximum element in the array
+// finding the minimum and maximum element in the array
 
 public class MIN_MAX {
     public static void main(String[] args){
 
         // Initializing the array and max with first element of array
-        int[] arr = {4 , 6 ,10 ,111 , 101 , 75 , 683 , 980} ; 
+        int[] arr = { -1, 4 , 6 ,10 ,111 , 101 , 75 , 683 , 980} ; 
         int min = arr[0] ; 
         int max = arr[0] ; 
 
