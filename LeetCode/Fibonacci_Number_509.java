72aea8594a664f5316d661a50a02474bb58a8cba
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2025-06-27 11:35:39 +0530

    Update Fibonacci_Number_509.java

--- LeetCode/Fibonacci_Number_509.java
+++ LeetCode/Fibonacci_Number_509.java
@@ -28,9 +28,15 @@ Constraints:
 */
 class Solution {
     public int fib(int n) {
-        if (n == 0 || n == 1){
-            return n;
+        if (n == 0) return 0;
+        if (n == 1) return 1 ;
+
+        int[] arr = new int[n+1];
+        arr[n-n] = 0;
+        arr[n-n+1] = 1;
+        for (int i = 2 ; i < n+1 ; i++ ){
+            arr[i] = arr[i-1]+arr[i-2];
         }
-        return fib(n-1)+fib(n-2) ; 
+        return arr[n];
     }
 }
