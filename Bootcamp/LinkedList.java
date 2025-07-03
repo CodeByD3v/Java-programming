f2083106d9f7518249371ac21658b7c59c980ee2
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2025-07-03 18:54:32 +0530

    Add files via upload

--- Bootcamp/LinkedList.java
+++ Bootcamp/LinkedList.java
@@ -1,27 +1,52 @@
-class Node{
-    int data;
-    Node next ; 
+class LinkedList {
 
-    Node(int val){
-        data = val;
-        next = null;
+    static class Listnode {
+        int val;
+        Listnode next;
+
+        Listnode(int val) {
+            this.val = val;
+            this.next = null;
+        }
     }
-}
 
-public class LinkedList {
-    public static void main(String[] args){
-        Node Node1 = new Node(12);
-        Node Node2 = new Node(18);
+    public static Listnode insertAtHead(Listnode head, int data) {
+        Listnode newnode = new Listnode(data);
+        newnode.next = head;
+        return newnode;
+    }
 
-        Node1.next = Node2;
+    public static Listnode insertAtTail(Listnode head, int data) {
+        Listnode newnode = new Listnode(data);
+        if (head == null) {
+            return newnode;
+        }
+        Listnode ptr = head;
+        while (ptr.next != null) {
+            ptr = ptr.next;
+        }
+        ptr.next = newnode;
+        return head;
+    }
 
-        System.out.println(Node1.data);
-        System.out.print(Node1.next);
-        System.out.println();
-
-        System.out.println(Node2.data);
-        System.out.println(Node2.next);
-
-        System.out.print(Node1.next.data);
+    public static void traverse(Listnode head) {
+        Listnode ptr = head;
+        while (ptr != null) {
+            System.out.print(ptr.val + "->");
+            ptr = ptr.next;
+        }
+    }
+    public static void main(String[] args) {
+        Listnode head = new Listnode(1);
+        traverse(head);
+        System.out.println("Insertion 2 at head ");
+        
+        head = insertAtHead(head, 2);
+        traverse(head);
+        System.out.println("");
+        traverse(head);
+        System.out.println("Insertion 3 at tail");
+        head = insertAtTail(head, 3);
+        traverse(head);
     }
 }
