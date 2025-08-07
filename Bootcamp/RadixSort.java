abe6d1a8cc0fa5232eefeda9ea079a725fb2ba9d
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2025-08-07 20:37:45 +0530

    RadixSort

---
+++ Bootcamp/RadixSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class RadixSort {
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int value : arr)
+            if (value > max)
+                max = value;
+        return max;
+    }
+
+    public static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int value : arr)
+            count[(value / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+        for (int exp = 1; max / exp > 0; exp *= 10)
+            countingSort(arr, exp);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        radixSort(arr);
+        System.out.println("Sorted using Radix Sort: " + Arrays.toString(arr));
+    }
+}
