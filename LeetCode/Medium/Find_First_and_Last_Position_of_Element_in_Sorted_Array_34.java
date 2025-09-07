9336aaeccbdc83da96d2a2e80d9940d7d9d0d3fc
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2025-09-07 19:15:38 +0530

    REORDERD

---
+++ LeetCode/Medium/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.java
@@ -0,0 +1,56 @@
+/*
+34. Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start = 0 ; 
+        int last = 0 ; 
+        Boolean found = false ; 
+        for (int i = 0 ; i < nums.length ; i++){
+            if (nums[i] == target){
+                start = i ;
+                found = true;
+                break;
+            }
+        }
+        for (int i = nums.length-1 ; i > 0 ; i --){
+            if ( nums[i] == target){
+                last = i;
+                break;
+            }
+        } 
+        
+        if (found == true){
+            return new int[] {start , last} ; 
+        }
+        else{
+        return new int[] {-1 , -1};
+        }
+    }
+}
